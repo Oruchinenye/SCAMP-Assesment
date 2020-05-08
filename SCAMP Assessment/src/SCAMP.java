319bd524d936b2b906893e98f7cb83c3e90b891c
Oru Chinenye <oruchinenye12@gmail.com>
2020-05-08 15:52:06 +0100

    SCAMP Assessment

---
+++ SCAMP Assessment/src/SCAMP.java
@@ -0,0 +1,16 @@
+public class SCAMP {
+	public static void main(String[] args) {
+		int m1=0,m2=1,m3,i,count=30;    
+		 System.out.print(m1+" "+m2);    
+		    
+		 for(i=2;i<count;++i)  
+		 {    
+		  m3=m1+m2;    
+		  System.out.print(" "+m3);    
+		  m1=m2;    
+		  m2=m3;  
+		
+	}
+
+}
+	}
